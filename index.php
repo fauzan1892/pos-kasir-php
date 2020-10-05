066b250f18061f1d36d93390f2bafd5c381ea0c9
Fauzan Falah <fauzan1892@codekop.com>
2020-10-06 00:00:47 +0700

    rev session error server hosting
    Solved for error session is Warning: session_start(): Cannot send session cookie - headers already sent by (output started at [duplicate]

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-
 <?php 
+	@ob_start();
 	session_start();
 
 		 if(!empty($_SESSION['admin'])){
--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+	@ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
+
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = md5(?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
+	?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -46,29 +70,6 @@
 	  	
 	  	</div>
 	  </div>
-	<?php
-		if(isset($_POST['proses'])){
-			require 'config.php';
-				
-			$user = strip_tags($_POST['user']);
-			$pass = strip_tags($_POST['pass']);
-
-			$sql = 'select member.*, login.user, login.pass
-					from member inner join login on member.id_member = login.id_member
-					where user =? and pass = md5(?)';
-			$row = $config->prepare($sql);
-			$row -> execute(array($user,$pass));
-			$jum = $row -> rowCount();
-			if($jum > 0){
-				session_start();
-				$hasil = $row -> fetch();
-				$_SESSION['admin'] = $hasil;
-				echo '<script>alert("Login Sukses");window.location="index.php"</script>';
-			}else{
-				echo '<script>alert("Login Gagal");history.go(-1);</script>';
-			}
-		}
-		?>
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
