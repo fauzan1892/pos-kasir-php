7416a56f3c5a9a93fed0eae86db2ce0fce361620
Fauzan Falah <fauzan1892@codekop.com>
2021-02-04 12:44:49 +0700

    fix search per hari

--- fungsi/view/view.php
+++ fungsi/view/view.php
@@ -181,8 +181,14 @@
 				$ex = explode('-', $hari);
 				$monthNum  = $ex[1];
 				$monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
-				$cek = $ex[2].' '.$monthName.' '.$ex[0];
-
+				if($ex[2] > 9)
+				{
+					$tgl = $ex[2];
+				}else{
+					$tgl1 = explode('0',$ex[2]);
+					$tgl = $tgl1[1];
+				}
+				$cek = $tgl.' '.$monthName.' '.$ex[0];
 				$param = "%{$cek}%";
 				$sql ="SELECT nota.* , barang.id_barang, barang.nama_barang,  barang.harga_beli, member.id_member,
 						member.nm_member from nota 
