12001a521f87234ad06b170119eb1b7fa254cb55
Fauzan Falah <fauzan1892@codekop.com>
2021-05-25 08:43:59 +0700

    Update tambah.php

--- fungsi/tambah/tambah.php
+++ fungsi/tambah/tambah.php
@@ -40,19 +40,35 @@ if(!empty($_SESSION['admin'])){
 	}
 	if(!empty($_GET['jual'])){
 		$id = $_GET['id'];
-		$kasir =  $_GET['id_kasir'];
-		$jumlah = '0';
-		$total = '0';
-		$tgl = date("j F Y, G:i");
 		
-		$data1[] = $id;
-		$data1[] = $kasir;
-		$data1[] = $jumlah;
-		$data1[] = $total;
-		$data1[] = $tgl;
-		$sql1 = 'INSERT INTO penjualan (id_barang,id_member,jumlah,total,tanggal_input) VALUES (?,?,?,?,?)';
-		$row1 = $config -> prepare($sql1);
-		$row1 -> execute($data1);
- 		echo '<script>window.location="../../index.php?page=jual&success=tambah-data"</script>';
+		// get tabel barang id_barang 
+		$sql = 'SELECT * FROM barang WHERE id_barang = ?';
+		$row = $config->prepare($sql);
+		$row->execute(array($id));
+		$hsl = $row->fetch();
+
+		if($hsl['stok'] > 0)
+		{
+			$kasir =  $_GET['id_kasir'];
+			$jumlah = 1;
+			$total = $hsl['harga_jual'];
+			$tgl = date("j F Y, G:i");
+			
+			$data1[] = $id;
+			$data1[] = $kasir;
+			$data1[] = $jumlah;
+			$data1[] = $total;
+			$data1[] = $tgl;
+			
+			$sql1 = 'INSERT INTO penjualan (id_barang,id_member,jumlah,total,tanggal_input) VALUES (?,?,?,?,?)';
+			$row1 = $config -> prepare($sql1);
+			$row1 -> execute($data1);
+
+			echo '<script>window.location="../../index.php?page=jual&success=tambah-data"</script>';
+
+		}else{
+			echo '<script>alert("Stok Barang Anda Telah Habis !");
+					window.location="../../index.php?page=jual#keranjang"</script>';
+		}
 	}
 }
