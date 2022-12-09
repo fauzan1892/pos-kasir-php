4448ed557fecf5c7c9ce8a88520d35ad61d0941f
Fauzan Falah <fauzancodekop@gmail.com>
2022-12-09 15:52:22 +0700

    Migrasi SB Admin Template Revisi - 1
    Belum Semua

--- admin/template/home.php
+++ admin/template/home.php
@@ -1,118 +1,95 @@
- <!--sidebar end-->
-      
-      <!-- **********************************************************************************************************************************************************
-      MAIN CONTENT
-      *********************************************************************************************************************************************************** -->
-      <!--main content start-->
-      <section id="main-content">
-          <section class="wrapper">
-
-              <div class="row">
-                  <div class="col-lg-9">
-					<div class="row" style="margin-left:1pc;margin-right:1pc;">
-				  <h1>DASHBOARD</h1>
-				  <hr>
-				  
-				  <?php 
-						$sql=" select * from barang where stok <= 3";
-						$row = $config -> prepare($sql);
-						$row -> execute();
-						$r = $row -> rowCount();
-						if($r > 0){
-					?>	
-					<?php
-							echo "
-							<div class='alert alert-warning'>
-								<span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'>$r</span> barang yang Stok tersisa sudah kurang dari 3 items. silahkan pesan lagi !!
-								<span class='pull-right'><a href='index.php?page=barang&stok=yes'>Tabel Barang <i class='fa fa-angle-double-right'></i></a></span>
-							</div>
-							";	
-						}
-					?>
-				  <?php $hasil_barang = $lihat -> barang_row();?>
-				  <?php $hasil_kategori = $lihat -> kategori_row();?>
-				  <?php $stok = $lihat -> barang_stok_row();?>
-				  <?php $jual = $lihat -> jual_row();?>
-                    <div class="row">
-                      <!--STATUS PANELS -->
-					  <div class="col-md-3">
-                      		<div class="panel panel-primary">
-                      			<div class="panel-heading">
-						  			<h5><i class="fa fa-desktop"></i> Nama Barang</h5>
-                      			</div>
-                      			<div class="panel-body">
-									<center><h1><?php echo number_format($hasil_barang);?></h1></center>
-								</div>
-								<div class="panel-footer">
-									<h4 style="font-size:15px;font-weight:700;"><a href='index.php?page=barang'>Tabel Barang <i class='fa fa-angle-double-right'></i></a></h4>
-								</div>
-	                      	</div><!--/grey-panel -->
-                      	</div><!-- /col-md-3-->
-                      <!-- STATUS PANELS -->
-                      	<div class="col-md-3">
-                      		<div class="panel panel-success">
-                      			<div class="panel-heading">
-						  			<h5><i class="fa fa-desktop"></i> Stok Barang</h5>
-                      			</div>
-                      			<div class="panel-body">
-									<center><h1><?php echo number_format($stok['jml']);?></h1></center>
-								</div>
-								<div class="panel-footer">
-									<h4 style="font-size:15px;font-weight:700;"><a href='index.php?page=barang'>Tabel Barang  <i class='fa fa-angle-double-right'></i></a></h4>
-								</div>
-	                      	</div><!--/grey-panel -->
-                      	</div><!-- /col-md-3-->
-                      <!-- STATUS PANELS -->
-                      	<div class="col-md-3">
-                      		<div class="panel panel-info">
-                      			<div class="panel-heading">
-						  			<h5><i class="fa fa-desktop"></i> Telah Terjual</h5>
-                      			</div>
-                      			<div class="panel-body">
-									<center><h1><?php echo number_format($jual['stok']);?></h1></center>
-								</div>
-								<div class="panel-footer">
-									<h4 style="font-size:15px;font-weight:700;font-weight:700;"><a href='index.php?page=laporan'>Tabel laporan  <i class='fa fa-angle-double-right'></i></a></h4>
-								</div>
-	                      	</div><!--/grey-panel -->
-                      	</div><!-- /col-md-3-->
-                      	<div class="col-md-3">
-                      		<div class="panel panel-danger">
-                      			<div class="panel-heading">
-						  			<h5><i class="fa fa-desktop"></i> Kategori Barang</h5>
-                      			</div>
-                      			<div class="panel-body">
-									<center><h1><?php echo number_format($hasil_kategori);?></h1></center>
-								</div>
-								<div class="panel-footer">
-									<h4 style="font-size:15px;font-weight:700;"><a href='index.php?page=kategori'>Tabel Kategori  <i class='fa fa-angle-double-right'></i></a></h4>
-								</div>
-	                      	</div><!--/grey-panel -->
-                      	</div><!-- /col-md-3-->
-					</div>
-				</div>
-           </div><!-- /col-lg-9 END SECTION MIDDLE -->
-                  
-      <!-- **********************************************************************************************************************************************************
-      RIGHT SIDEBAR CONTENT
-      *********************************************************************************************************************************************************** -->                  
-                  
-			<div class="col-lg-3 ds">
-				<div id="calendar" class="mb">
-					<div class="panel green-panel no-margin">
-						<div class="panel-body">
-							<div id="date-popover" class="popover top" style="cursor: pointer; disadding: block; margin-left: 33%; margin-top: -50px; width: 175px;">
-								<div class="arrow"></div>
-								<h3 class="popover-title" style="disadding: none;"></h3>
-								<div id="date-popover-content" class="popover-content"></div>
-							</div>
-							<div id="my-calendar"></div>
-						</div>
-					</div>
-				</div><!-- / calendar -->
-			  </div><!-- /col-lg-3 -->
-		  </div><! --/row -->
-		 <div class="clearfix" style="padding-top:18%;"></div>
-	  </section>
-  </section>
-
+<h3>Dashboard</h3>
+<br/>
+<?php 
+	$sql=" select * from barang where stok <= 3";
+	$row = $config -> prepare($sql);
+	$row -> execute();
+	$r = $row -> rowCount();
+	if($r > 0){
+?>
+<?php
+		echo "
+		<div class='alert alert-warning'>
+			<span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'>$r</span> barang yang Stok tersisa sudah kurang dari 3 items. silahkan pesan lagi !!
+			<span class='pull-right'><a href='index.php?page=barang&stok=yes'>Tabel Barang <i class='fa fa-angle-double-right'></i></a></span>
+		</div>
+		";	
+	}
+?>
+<?php $hasil_barang = $lihat -> barang_row();?>
+<?php $hasil_kategori = $lihat -> kategori_row();?>
+<?php $stok = $lihat -> barang_stok_row();?>
+<?php $jual = $lihat -> jual_row();?>
+<div class="row">
+    <!--STATUS cardS -->
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h6 class="pt-2"><i class="fas fa-cubes"></i> Nama Barang</h6>
+            </div>
+            <div class="card-body">
+                <center>
+                    <h1><?php echo number_format($hasil_barang);?></h1>
+                </center>
+            </div>
+            <div class="card-footer">
+                <a href='index.php?page=barang'>Tabel
+                    Barang <i class='fa fa-angle-double-right'></i></a>
+            </div>
+        </div>
+        <!--/grey-card -->
+    </div><!-- /col-md-3-->
+    <!-- STATUS cardS -->
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h6 class="pt-2"><i class="fas fa-chart-bar"></i> Stok Barang</h6>
+            </div>
+            <div class="card-body">
+                <center>
+                    <h1><?php echo number_format($stok['jml']);?></h1>
+                </center>
+            </div>
+            <div class="card-footer">
+                <a href='index.php?page=barang'>Tabel
+                    Barang <i class='fa fa-angle-double-right'></i></a>
+            </div>
+        </div>
+        <!--/grey-card -->
+    </div><!-- /col-md-3-->
+    <!-- STATUS cardS -->
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h6 class="pt-2"><i class="fas fa-upload"></i> Telah Terjual</h6>
+            </div>
+            <div class="card-body">
+                <center>
+                    <h1><?php echo number_format($jual['stok']);?></h1>
+                </center>
+            </div>
+            <div class="card-footer">
+                <a href='index.php?page=laporan'>Tabel
+                    laporan <i class='fa fa-angle-double-right'></i></a>
+            </div>
+        </div>
+        <!--/grey-card -->
+    </div><!-- /col-md-3-->
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h6 class="pt-2"><i class="fa fa-bookmark"></i> Kategori Barang</h6>
+            </div>
+            <div class="card-body">
+                <center>
+                    <h1><?php echo number_format($hasil_kategori);?></h1>
+                </center>
+            </div>
+            <div class="card-footer">
+                <a href='index.php?page=kategori'>Tabel
+                    Kategori <i class='fa fa-angle-double-right'></i></a>
+            </div>
+        </div>
+        <!--/grey-card -->
+    </div><!-- /col-md-3-->
+</div>
--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -1,61 +1,126 @@
-   
-      <!-- **********************************************************************************************************************************************************
-      MAIN SIDEBAR MENU
-      *********************************************************************************************************************************************************** -->
-      <!--sidebar start-->
+<!--sidebar start-->
 <?php 
-  $id = $_SESSION['admin']['id_member'];
-  $hasil_profil = $lihat -> member_edit($id);
+    $id = $_SESSION['admin']['id_member'];
+    $hasil_profil = $lihat -> member_edit($id);
 ?>
-      <aside>
-          <div id="sidebar"  class="nav-collapse">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
-              
-              	  <p class="centered"><a><img src="assets/img/user/<?php echo $hasil_profil['gambar'];?>" class="img-circle" width="100" height="100"></a></p>
-              	  <h5 class="centered"><?php echo $hasil_profil['nm_member'];?></h5>
-              	  <h5 class="centered">( <?php echo $hasil_profil['NIK'];?> )</h5>
-              	  	
-                  <li class="mt">
-                      <a href="index.php">
-                          <i class="fa fa-dashboard"></i>
-                          <span>Dashboard</span>
-                      </a>
-                  </li>
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-                  <li class="sub-menu">
-                      <a href="javascript:;">
-                          <i class="fa fa-desktop"></i>
-                          <span>Master <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="index.php?page=barang">Barang</a></li>
-                          <li><a  href="index.php?page=kategori">Kategori</a></li>
-                          <li><a  href="index.php?page=user">User</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>Transaksi <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="index.php?page=jual">Transaksi Jual</a></li>
-                          <li><a  href="index.php?page=laporan">Laporan Penjualan</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-cog"></i>
-                          <span>Setting <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a href="index.php?page=pengaturan">Pengaturan Toko</a></li>
-                      </ul>
-                  </li>
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
-      <!--sidebar end-->
-      
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-cash-register"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">POS Codekop<sup></sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <!-- Heading -->
+    <!-- <div class="sidebar-heading">
+           Master
+       </div> -->
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Data Master</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                <a class="collapse-item" href="index.php?page=barang">Barang</a>
+                <a class="collapse-item" href="index.php?page=kategori">Kategori</a>
+                <!-- <a class="collapse-item" href="index.php?page=user">User</a> -->
+            </div>
+        </div>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
+            aria-controls="collapse3">
+            <i class="fas fa-fw fa-desktop"></i>
+            <span>Transaksi</span>
+        </a>
+        <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                <a class="collapse-item" href="index.php?page=jual">Transaksi Jual</a>
+                <a class="collapse-item" href="index.php?page=laporan">Laporan Penjualan</a>
+            </div>
+        </div>
+    </li>
+    <hr class="sidebar-divider">
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php?page=pengaturan">
+            <i class="fas fa-fw fa-cogs"></i>
+            <span>Pengaturan Toko</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+            <p class="d-lg-block d-none mt-3"><?php echo $toko['nama_toko'];?>, <?php echo $toko['alamat_toko'];?></p>
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- <div class="topbar-divider d-none d-sm-block"></div> -->
+                <!-- Topbar Search -->
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <img class="img-profile rounded-circle"
+                            src="assets/img/user/<?php echo $hasil_profil['gambar'];?>">
+                        <span
+                            class="mr-2 d-none d-lg-inline text-gray-600 small ml-2"><?php echo $hasil_profil['nm_member'];?></span>
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                        aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="index.php?page=user">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout.php">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <!-- End of Topbar -->
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
