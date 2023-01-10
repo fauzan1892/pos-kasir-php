411e577f9e24e8294b841f9bdc65d328d4b524ba
jagruti4699 <80021688+jagruti4699@users.noreply.github.com>
2023-01-10 12:52:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -19,9 +19,9 @@ date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 	// sesuaikan dengan server anda
-	$host 	= 'localhost'; // host server
-	$user 	= 'root';  // username server
-	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
+	$host 	= 'testjag.mysql.database.azure.com'; // host server
+	$user 	= 'jagruti';  // username server
+	$pass 	= 'Neosoft123'; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
 	
 	try{
